241af42db94cff7c4c806451b4462c39655bebc1
Cory <corydeuschle92@gmail.com>
2024-12-05 14:23:14 -0600

    Finished react part 1 up to exercises

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Books To Read";
+   let book1 = "https://images.penguinrandomhouse.com/cover/9780307387899";
+   let book2 = "https://creativeaction.network/cdn/shop/products/the-collected-tales-of-edgar-allan-poe-by-adam-s-doyle-12-by-18-print-unframed-print-recovering-the-classics-6922521923_2048x.jpg?v=1600034116";
+   let book3 = "https://m.media-amazon.com/images/I/81tBoQP5V+L.jpg";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="Image showing the book cover for The Road" />
+         <img src={book2} alt="Image showing the book cover for a collection of works of Edgar Allan Poe" />
+         <img src={book3} alt="Image showing the book cover for The Eye of The World" />
       </div>      
    );
 }
